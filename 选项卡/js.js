491cf165edd7d05567a1f5ef8c8ad72192292546
ansouth <ansouth@qq.com>
2018-04-10 23:24:37 +0800

    addSelectCard

---
+++ 选项卡/js.js
@@ -0,0 +1,22 @@
+function $(id){
+    return typeof id==='string'?document.getElementById(id):id;
+}
+window.onload=function(){
+    var navs=$('tab-nav').getElementsByTagName('li');
+    var divs=$('tab-contain').getElementsByTagName('div');
+    // alert(divs.length);
+    if(navs.length!=divs.length){
+        return;
+    }
+    for(var i=0;i<navs.length;i++){
+        navs[i].title=i;
+        navs[i].onclick=function(){
+            for(var j=0;j<navs.length;j++){
+                navs[j].className="";
+                divs[j].style.display="none";
+            }
+            this.className="active";
+            divs[this.title].style.display="block";
+        }
+    }
+}
